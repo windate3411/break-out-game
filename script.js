1888120baec74c041775b1bce4f637d402a24d46
Danny <windate3411@gmail.com>
2020-02-20 14:15:04 +0800

    draw ball and paddle on canvas

--- script.js
+++ script.js
@@ -2,6 +2,52 @@
 const rulesBtn = document.getElementById('show-rules')
 const closeBtn = document.getElementById('close-btn')
 const rules = document.getElementById('rules')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+
+// Create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4
+}
+
+// create paddle on canvas
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  dx: 0,
+  speed: 8
+}
+
+function drawPaddle() {
+  ctx.beginPath()
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+  ctx.fillStyle = '#0095dd'
+  ctx.fill()
+  ctx.closePath()
+}
+
+// draw ball on canvas
+function drawBall() {
+  ctx.beginPath()
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+  ctx.fillStyle = '#0095dd'
+  ctx.fill()
+  ctx.closePath()
+}
+
+// draw everything 
+function draw() {
+  drawPaddle()
+  drawBall()
+}
 
 // Rules and Close event listener
 rulesBtn.addEventListener('click', () => {
@@ -11,3 +57,5 @@ rulesBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   rules.classList.remove('show')
 })
+
+draw()
